105fbd9726aa746674b7209f396f5cb346ae00a5
Logan Smith <logsmith06@gmail.com>
2021-01-05 18:12:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,11 @@ function updateDriverWithKeyAndValue(driver, key, value){
   const newDriver = {...driver};
   newDriver[key] = value;
   return newDriver;
-   // return Object.assign(driver, {[key]:value})
+
+}
+
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+  driver[key] = value;
+  return driver
 }
