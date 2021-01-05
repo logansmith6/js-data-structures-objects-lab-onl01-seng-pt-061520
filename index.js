94a748b09ef90f3d24983b0426d59729dcdf770e
Logan Smith <logsmith06@gmail.com>
2021-01-05 18:05:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Write your solution in this file!
 
 function updateDriverWithKeyAndValue(obj, key, value){
-  const driver = {...obj};
-  driver[key] = value;
-  return driver;
-   // return Object.assign(driver, obj, {[key]:value})
+//  const driver = {...obj};
+//  driver[key] = value;
+//  return driver;
+   return Object.assign(driver, obj, {[key]:value})
 }
